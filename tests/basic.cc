9a423193eef0070c5ccb2cdd607b66984e9aea9a
Nicu Tofan <nicu.tofan@gmail.com>
2015-03-30 04:48:35 +0300

    ls test

--- tests/basic.cc
+++ tests/basic.cc
@@ -1,8 +1,11 @@
 #include <gtest/gtest.h>
 #include <procrun/procrun.h>
+#include <QApplication>
+
+#include <procrun/procrun.h>
 
 class TestUsage : public ::testing::Test {
-
+    QApplication * app;
 public:
 
     TestUsage () {}
@@ -11,14 +14,33 @@ public:
 
     void SetUp(){
 
+        int argc = 1;
+        char *argv[] = {strdup("test")};
+        app = new  QApplication(argc, argv);
     }
     void TearDown(){
-
+        delete app;
     }
 
 };
 
 TEST_F(TestUsage, initial_state) {
+    ProcRun pr;
+    QStringList arguments;
+    QStringList prog_input;
 
+    pr.setProgram ("ls");
+    pr.setArguments (arguments);
+    pr.perform (prog_input);
+
+    //EXPECT_TRUE(pr.runDuration() > 0);
+    //EXPECT_TRUE(pr.durationInSeconds() > 0);
+    //EXPECT_TRUE(pr.durationInMiliSeconds() > 0);
+    EXPECT_TRUE(pr.std_err_output_.count() == 0);
+    EXPECT_TRUE(pr.std_out_output_.count() > 0);
+    EXPECT_TRUE(pr.all_output_ == pr.std_out_output_);
+    EXPECT_TRUE(pr.b_started_);
+    EXPECT_TRUE(pr.errors_.count() == 0);
+    EXPECT_TRUE(pr.states_.count() > 0);
 }
 
